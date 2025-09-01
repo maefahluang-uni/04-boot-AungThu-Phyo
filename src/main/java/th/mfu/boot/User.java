cedeb6f4a60527a1fb592b8696354f834e50b605
Aung Thu Phyo <6731503046@lamduan.mfu.ac.th>
2025-09-01 09:21:05 +0000

    submit

--- src/main/java/th/mfu/boot/User.java
+++ src/main/java/th/mfu/boot/User.java
@@ -1,6 +1,31 @@
 package th.mfu.boot;
 
 public class User {
+//     username: a unique identifier for the user
+// displayname: the user's first and last name
+// email: the user's email address
+    private String username;
+    private String displayname;
+    private String email;
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getDisplayname() {
+        return displayname;
+    }
+    public void setDisplayname(String displayname) {
+        this.displayname = displayname;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
 
     
 }
